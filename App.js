bf835c25fc2fe18a9d912928361ef2f4f5ecd1fb
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-10-31 20:09:04 +0100

    basic navigation

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
-  return (
-    <View>
-      
-    </View>
-  );
+  return <PlacesNavigator />;
 }
